20e615b8a4c589dd5edec610823572d1455d83ce
laag-dev <laag.08@gmail.com>
2024-06-20 01:07:43 -0500

    Set up TriLine game project

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import view.FormInit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        FormInit formInit = new FormInit();
+        
+        
+        formInit.setVisible(true);
+        
+    }
+}
+
